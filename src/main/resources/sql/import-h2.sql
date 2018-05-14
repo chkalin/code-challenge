82329e213dc227306dc38e49dbcc411307e2100c
TomLin <382052750@qq.com>
2018-05-14 21:08:10 +0800

    Update import-h2.sql
    Delete one wrong record in follow table.

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -113,5 +113,3 @@ INSERT INTO COMMENT (post_id, user_id, body, create_date)
 VALUES (6, 3,
         '"commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-
-INSERT INTO FOLLOW (SORT_ID, FOLLOW_USERNAME, USERNAME) VALUES(1, 'johndoe', 'user');
